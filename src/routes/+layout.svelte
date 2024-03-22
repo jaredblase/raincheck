524cc28c7ed1fbf00c6a1282b46b4a450fa1fdb8
Jared Sy <jaredblase@gmail.com>
2024-03-22 11:53:49 +0800

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.pcss';
+</script>
+
+<svelte:head>
+	<title>Raincheck | Home</title>
+</svelte:head>
+
+<header class="container flex h-16 items-center">
+	<p class="text-3xl font-medium text-primary">raincheck</p>
+</header>
+
+<main>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<section class="container">
+	<h1 class="text-secondary text-5xl font-medium">Hi! Let's set up a new event.</h1>
+
+	<form action="">
+		
+	</form>
+</section>
